0646e9ba0cee4b1c53929f10b07ac298a0b9e47c
Pepe García <pepe@pepegar.com>
2020-07-20 10:40:56 +0100

    add try_spyder.py file

---
+++ code/session1/try_spyder.py
@@ -0,0 +1,12 @@
+
+
+# Lines starting with the # sign in Python are called comments,
+# They're not evaluated by Python.
+#
+# They're useful for the programmer to write documentation.
+
+#%%
+
+name = input("What is your name? ")
+
+print("Hello, " + name)
