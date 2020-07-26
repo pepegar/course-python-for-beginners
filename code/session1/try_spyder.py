d3b2ca84fb590cdd569342b8879f98e6a4cd9558
Pepe García <pepe@pepegar.com>
2020-07-26 22:34:09 +0100

    changes after first session

--- code/session1/try_spyder.py
+++ code/session1/try_spyder.py
@@ -5,8 +5,67 @@
 #
 # They're useful for the programmer to write documentation.
 
+
+#%%
+
+print(8 * 8)
+
+#%%
+
+print(3 ** 5)
+print("Na" * 16 + " Batman")
+
+#%%
+
+# We should strive for DRY
+print(3.14 * 2 ** 2)
+print(3.14 * 4 ** 2)
+
+#%%
+
+print(pi)
+
+#%%
+
+pi = 3.14
+
+print(pi * 2 ** 2)
+print(pi * 4 ** 2)
+
+#%%
+
+acceleration_of_gravity = 9.8
+three_times_three = 9
+our_variable = "hello"
+
+
 #%%
 
 name = input("What is your name? ")
 
 print("Hello, " + name)
+
+#%%
+
+age = input("tell me your age: ")
+
+age_in_ten_years = int(age) + 10
+
+print("in 10 years you will be " + str(age_in_ten_years))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
